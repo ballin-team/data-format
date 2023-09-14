66cfb8629ba6098c0305fff30f9daba0eea4d6ce
Renan <renan.soares.francisco@gmail.com>
2023-09-13 22:49:14 -0300

    feat(test): adding execution tests for snakeToCamel func

--- __tests__/camelToSnake.spec.ts
+++ __tests__/camelToSnake.spec.ts
@@ -1,26 +1,26 @@
-import { snakeToCamel } from '../src';
+import { camelToSnake } from '../src';
 import * as helper_functions from '../src/helpers';
-import { validSnakeDummies, validSnakeDummy } from './dummies/snakeToCamel.dummy';
+import { validCamelDummies, validCamelDummy } from './dummies/camelToSnake.dummy';
 
 describe('Input validations', () => {
-  it.each(['snake_case', null, undefined, true])('Should return a non-changed value if the argument is %p', input => {
-    expect(snakeToCamel(input)).toEqual(input);
+  it.each(['camelCase', null, undefined, true])('Should return a non-changed value if the argument is %p', input => {
+    expect(camelToSnake(input)).toEqual(input);
   });
 
   it('Should accept empty object', () => {
     const input = {};
-    expect(snakeToCamel(input)).toEqual(input);
+    expect(camelToSnake(input)).toEqual(input);
   });
 
   it('Should accept empty array', () => {
     const input: never[] = [];
-    expect(snakeToCamel(input)).toEqual(input);
+    expect(camelToSnake(input)).toEqual(input);
   });
 
-  it('Should call modifyObjectKeysWithCache if useCache is true', () => {``
+  it('Should call modifyObjectKeysWithCache if useCache is true', () => {
     const modifyObjectKeysWithCacheSpy = jest.spyOn(helper_functions, 'modifyObjectKeysWithCache');
     const modifyObjectKeysSpy = jest.spyOn(helper_functions, 'modifyObjectKeys');
-    snakeToCamel(validSnakeDummy, true);
+    camelToSnake(validCamelDummy, true);
     expect(modifyObjectKeysWithCacheSpy).toBeCalledTimes(1);
     expect(modifyObjectKeysSpy).toBeCalledTimes(0);
   });
@@ -28,7 +28,7 @@ describe('Input validations', () => {
   it('Should call modifyObjectKeys if useCache is false', () => {
     const modifyObjectKeysSpy = jest.spyOn(helper_functions, 'modifyObjectKeys');
     const modifyObjectKeysWithCacheSpy = jest.spyOn(helper_functions, 'modifyObjectKeysWithCache');
-    snakeToCamel(validSnakeDummy, false);
+    camelToSnake(validCamelDummy, false);
     expect(modifyObjectKeysSpy).toBeCalledTimes(1);
     expect(modifyObjectKeysWithCacheSpy).toBeCalledTimes(0);
   });
@@ -36,18 +36,18 @@ describe('Input validations', () => {
   it('Should call modifyObjectKeys if omit useCache argument', () => {
     const modifyObjectKeysSpy = jest.spyOn(helper_functions, 'modifyObjectKeys');
     const modifyObjectKeysWithCacheSpy = jest.spyOn(helper_functions, 'modifyObjectKeysWithCache');
-    snakeToCamel(validSnakeDummy);
+    camelToSnake(validCamelDummy);
     expect(modifyObjectKeysSpy).toBeCalledTimes(1);
     expect(modifyObjectKeysWithCacheSpy).toBeCalledTimes(0);
   });
 });
 
 describe('Execution validation', () => {
-  it.each(validSnakeDummies)('Should a success response %p', (_, input, response) => {
-    expect(snakeToCamel(input)).toEqual(response);
+  it.each(validCamelDummies)('Should a success response %p', (_, input, response) => {
+    expect(camelToSnake(input)).toEqual(response);
   });
 
-  it.each(validSnakeDummies)('Should a success response %p (cache version)', (_, input, response) => {
-    expect(snakeToCamel(input, true)).toEqual(response);
+  it.each(validCamelDummies)('Should a success response %p (cache version)', (_, input, response) => {
+    expect(camelToSnake(input, true)).toEqual(response);
   });
 });
--- __tests__/dummies/index.ts
+++ __tests__/dummies/index.ts
@@ -1,15 +1,5 @@
-export const obj = {
-  test_key_1: true,
-  test_key_2: true,
-  test_key_3: true,
-  test_key_4: true,
-  test_key_5: true,
-  test_key_6: true,
-  test_key_7: true,
-  test_key_8: true,
-  test_key_9: true,
-  test_key_10: true,
-};
+export * from './camelToSnake.dummy';
+export * from './snakeToCamel.dummy';
 
 export const objNested = {
   test_key_1: true,
