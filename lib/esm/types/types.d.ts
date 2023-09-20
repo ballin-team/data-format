10794ff0004c28c3e1b4424dc1f9aab63ec826cd
Renan <renan.soares.francisco@gmail.com>
2023-09-20 18:39:42 -0300

    feat: improving readme

---
+++ lib/esm/types/types.d.ts
@@ -0,0 +1,9 @@
+export type SnakeToCamelCase<S extends string> = S extends `${infer T}_${infer U}` ? `${T}${Capitalize<SnakeToCamelCase<U>>}` : S;
+export type CamelToSnakeCase<S extends string> = S extends `${infer T}${infer U}` ? `${T extends Capitalize<T> ? '_' : ''}${Lowercase<T>}${CamelToSnakeCase<U>}` : S;
+export type SnakeToCamelCaseNested<T> = T extends object ? {
+    [K in keyof T as SnakeToCamelCase<K & string>]: SnakeToCamelCaseNested<T[K]>;
+} : T;
+export type CamelCaseToSnakeNested<T> = T extends object ? {
+    [K in keyof T as CamelToSnakeCase<K & string>]: CamelCaseToSnakeNested<T[K]>;
+} : T;
+//# sourceMappingURL=types.d.ts.map
