a3e779b3f27c2d597a916d2deb5b82c69a841858
Renan <renan.soares.francisco@gmail.com>
2023-09-20 22:38:20 -0300

    fix

--- lib/cjs/types/helpers.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const isValidDate: (input: unknown) => boolean;
-export declare const convertSnakeToCamel: (input: string) => string;
-export declare const convertCamelToSnake: (input: string) => string;
-export declare const modifyObjectKeys: <T>(input: T, formatter: (word: string) => string) => any;
-export declare const modifyObjectKeysWithCache: <T>(input: T, formatter: (word: string) => string) => any;
-//# sourceMappingURL=helpers.d.ts.map
